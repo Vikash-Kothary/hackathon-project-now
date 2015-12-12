e84277c474548023aafae3d6be6e6183c86251c5
luxman <luxman.elangeswaralingam@kcl.ac.uk>
2015-12-12 23:51:07 +0000

    added the tablayout and made it responsive

--- app/src/main/java/com/hackkings/now/MainActivity.java
+++ app/src/main/java/com/hackkings/now/MainActivity.java
@@ -1,15 +1,24 @@
 package com.hackkings.now;
 
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +26,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createTabs();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void createTabs() {
+
+        tabs = (TabLayout) findViewById(R.id.tabLayout);
+        tabs.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabs.addTab(tabs.newTab().setText("Own"));
+        tabs.addTab(tabs.newTab().setText("Joined"));
+        tabs.addTab(tabs.newTab().setText("Nearby"));
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, new Own_fragment()).commit();
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTabSelected(TabLayout.Tab tab) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment fragment = null;
+
+                int tabselected = tabs.getSelectedTabPosition();
+                switch (tabselected) {
+                    case 0:
+                        fragment = new Own_fragment();
+                        break;
+                    case 1:
+                        fragment = new Joined_fragment();
+                        break;
+                    case 2:
+                        fragment = new Nearby_fragment();
+                        break;
+                }
+                if (fragment != null) {
+                    fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+                    fragmentTransaction.commit();
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
+
+    }
+
+    public void changeFragment() {
+
     }
 
     @Override
@@ -49,4 +105,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
