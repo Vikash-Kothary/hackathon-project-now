9e89c83a69f51be20a6c567a86fe0ef39e36b93d
Suhaila95 <morabysuhaila@hotmail.com>
2015-12-12 20:44:57 +0000

    java for toolbar

--- app/src/main/java/com/hackkings/now/ViewEventActivity.java
+++ app/src/main/java/com/hackkings/now/ViewEventActivity.java
@@ -1,7 +1,9 @@
 package com.hackkings.now;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +13,17 @@ public class ViewEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_event);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+//        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
 
