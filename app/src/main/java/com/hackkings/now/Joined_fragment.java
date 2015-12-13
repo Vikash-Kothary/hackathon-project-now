0c000b9043096a72489c05279efe7232782f3bc8
luxman <luxman.elangeswaralingam@kcl.ac.uk>
2015-12-13 11:59:25 +0000

    final changes

--- app/src/main/java/com/hackkings/now/Joined_fragment.java
+++ app/src/main/java/com/hackkings/now/Joined_fragment.java
@@ -1,15 +1,21 @@
 package com.hackkings.now;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Joined_fragment extends Fragment {
     ListView listView;
+    List<String > joinedEvents;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +26,26 @@ public class Joined_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.joined_fragment, container, false);
+        View rootview = inflater.inflate(R.layout.joined_fragment , container, false);
+        if (rootview != null){
+            listView = (ListView)rootview.findViewById(R.id.listviewJoinedFragment);
+            listView.setVerticalScrollBarEnabled(true);
+
+            joinedEvents = new ArrayList<String>();
+            addEvents();
+        }
+        return rootview;
+
+    }
+
+    public void addEvents (){
+        joinedEvents.add("House Party");
+        joinedEvents.add("Football Match");
+        joinedEvents.add("Cinam with mates");
+        joinedEvents.add("Christmas diner");
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1 , joinedEvents);
+        listView.setAdapter(arrayAdapter);
+
     }
 
 }
--- app/src/main/java/com/hackkings/now/Nearby_fragment.java
+++ app/src/main/java/com/hackkings/now/Nearby_fragment.java
@@ -7,11 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Nearby_fragment extends Fragment {
     ListView listView;
+    List<String> nearbyevents;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +28,22 @@ public class Nearby_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.nearby_fragment , container , false);
+        if (rootview != null){
+            listView = (ListView)rootview.findViewById(R.id.listviewNearbyFragment);
+            listView.setVerticalScrollBarEnabled(true);
+            nearbyevents = new ArrayList<String>();
+            addEvents();
+        }
         return rootview;
+    }
+
+    public void addEvents () {
+        nearbyevents.add(" Luxman's event");
+        nearbyevents.add("Vikaesh's event");
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1 , nearbyevents);
+        listView.setAdapter(arrayAdapter);
+
+
     }
 
 
