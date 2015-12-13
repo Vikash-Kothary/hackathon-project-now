0721a6728e21e2c8f5bc5e98121b9baf9d864a43
VikashKothary <kothary.vikash@gmail.com>
2015-12-13 04:22:22 +0000

    Merged completed

--- app/src/main/java/com/hackkings/now/MainActivity.java
+++ app/src/main/java/com/hackkings/now/MainActivity.java
@@ -1,62 +1,66 @@
 package com.hackkings.now;
 
 import android.content.Intent;
-import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TabHost;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private TabLayout tabs;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        createTabs();
+
+        setupToolbar();
+        setupFab();
+        setupTablayout();
 
     }
 
-    public void createTabs() {
+    public void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 
-        tabs = (TabLayout) findViewById(R.id.tabLayout);
-        tabs.setTabGravity(TabLayout.GRAVITY_FILL);
-        tabs.addTab(tabs.newTab().setText("Own"));
-        tabs.addTab(tabs.newTab().setText("Joined"));
-        tabs.addTab(tabs.newTab().setText("Nearby"));
-
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragmentContainer, new Own_fragment()).commit();
-
-        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+    public void setupFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CreateEventActivity.class);
                 startActivity(intent);
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Intent intent = new Intent(getApplicationContext(), ViewEventActivity.class);
-                startActivity(intent);
+            }
+        });
+    }
+
+    public void setupTablayout() {
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Own"));
+        tabLayout.addTab(tabLayout.newTab().setText("Joined"));
+        tabLayout.addTab(tabLayout.newTab().setText("Nearby"));
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, new Own_fragment()).commit();
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 Fragment fragment = null;
 
-                int tabselected = tabs.getSelectedTabPosition();
+                int tabselected = tabLayout.getSelectedTabPosition();
                 switch (tabselected) {
                     case 0:
                         fragment = new Own_fragment();
@@ -88,10 +92,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void changeFragment() {
-
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -104,13 +104,10 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
---
+++ app/src/main/java/com/hackkings/now/Own_fragment.java
@@ -0,0 +1,7 @@
+package com.hackkings.now;
+
+/**
+ * Created by Vikash Kothary on 13-Dec-15.
+ */
+public class Own_fragment extends android.support.v4.app.Fragment {
+}
